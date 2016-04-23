1c9953af071b72f74165b7132bd1e630e77fbaa5
John Carpenter <johncarpenter281@gmail.com>
2016-04-23 15:30:29 -0500

    implemented endpoints

--- server/routes/applicant.js
+++ server/routes/applicant.js
@@ -1,19 +1,76 @@
-module.exports = function(router) {
+var Applications = require('../models/Applications.js')
+var express = require('express');
+var bodyParser = require('body-parser');
 
-	router.use(function(req,res,next) {
+module.exports = function(router) {
+  //var app = express();
+  router.use(bodyParser.json()); // support json encoded bodies
+  //app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+  //var router = express.Router();
+
+  router.get('/appsbyuser', function(req, res){
+    console.log('---appsbyuser:received GET, query='+JSON.stringify(req.query));
+    var rq = req.query;
+    if (rq && rq.userID) {
+      console.log("request for userId = ",rq.userID);
+      Applications.getAppsByUser(rq.userID)
+      .then(function(data){
+        console.log("returning application data", data);
+        res.status(200).send(JSON.stringify(data));
+      })
+      .catch(function(err){
+        console.log("could not get application data for userID "+rq.userID+", err:", err);
+        res.status(400).send(err);
+      })
+    } else {
+      console.log("must supply userID in query string");
+      res.status(400).send("must supply userID in query string");       
+    }
+  }); 
+
+  router.post('/submitapp', function(req, res) {
+    console.log('received submit application POST, body:',req.body);
+    if(! req || !req.body) {
+      res.status(400).send("/submitapp expected a body object");
+    } else {
+      console.log("body:",req.body);
+      Applications.submit(req.body)
+      .then(function(data){
+        console.log("application successfully submitted")
+        res.status(200).send("success!");
+      })
+      .catch(function(err){
+        console.log("application submission failed, err:",err);
+        res.status(400).send("application submission failed:",err);
+      })
+    }
+  });
+
+	//router.use(function(req,res,next) {
 		//check to see if applicant
 		//if applicant res.next()
 		// }
-
+    //next();
 		// res.redirect('/job')
-	});
+	//});
 
 	//routes:
+  //router.get('/appsbyuser', function(req, res) {
+  //  console.log("applicant:appsbyuser:responding with appsbyuser data");
+  //  res.status(200).send("applicant:appsbyuser");
+    /*
+    console.log('---received appsbyuser GET, query='+JSON.stringify(req.query));
+    var rq = req.query;
+
+    */
+  //});
 
 
 	//catch all
-	router.get('/*', function(req, res) {
+	router.get('/*', function(req, res) { 
 		res.redirect('/job')
 	});
-
+  router.post('/*', function(req, res) {
+    res.redirect('/job')
+  });
 }
