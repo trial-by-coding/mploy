055e392d5e5eed960ddab5587552ba0185e72ba5
frankievx <frankievx@gmail.com>
2016-04-23 16:36:18 -0500

    Made ApplicationActions and JobActions AJAX calls

---
+++ src/jsx/app/redux/actions/jobActions.js
@@ -0,0 +1,36 @@
+import Promise from 'es6-promise';
+import fetch from 'isomorphic-fetch';
+
+
+function getJobs(jobID) {
+	return new Promise(function(resolve, reject) {
+		fetch('/job' + jobID)
+			.then(function(response) {
+				if (response.status >= 400) {
+	        throw new Error("Bad response from server");
+	      }
+	      console.log(response);
+	      resolve(response);
+			})
+	})
+}
+
+function apply(data) {
+	return new Promise(function(resolve, reject) {
+		$.ajax({
+			type: 'POST',
+			url: '/job',
+			data: data,
+			success: function(resp) {
+				if (response.status >= 400) {
+	        throw new Error("Bad response from server");
+	      }
+	      resolve(resp);
+			}
+		})
+	})
+}
+
+module.exports = {
+	getJobs: getJobs
+}
