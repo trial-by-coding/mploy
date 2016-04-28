0f1a6b7685a6fa00e40174194cd4a746e9c621b8
frankievx <frankievx@gmail.com>
2016-04-28 15:15:59 -0500

    Add GET_UNCONSIDERED case

--- src/jsx/app/redux/reducers/empdash_reducer.js
+++ src/jsx/app/redux/reducers/empdash_reducer.js
@@ -3,3 +3,24 @@ import {GET_UNCONSIDERED,
 			  GET_INTERVIEWS,
 			  GET_OFFERS } from '../actions/actionTypes';
 
+const initialState = {
+      unconsidered: [],
+      considered: [],
+      interviews: [],
+      offers: []
+    }
+
+function dashboard(state = initialState, action) {
+	console.log("state in dashboard reducer = ", state);
+	switch (action.type) {
+		case: GET_UNCONSIDERED:
+			console.log("unconsidered:", action.payload)
+			const data = action.payload.data;
+			return Object.assign({}, state, {
+				items: data
+			})
+		default:
+			return state;
+		}
+	}
+}
