15d5c0ace88ab6a48061b31c51e751f678a6fc6a
frankievx <frankievx@gmail.com>
2016-04-29 11:01:52 -0500

    Fixed endpoint appsbyjob

--- server/routes/employer.js
+++ server/routes/employer.js
@@ -63,6 +63,26 @@ module.exports = function(router) {
     }
   });
 
+  router.get('/appsbyjob', function(req, res){
+    console.log('---appsbystatus:received GET, query='+JSON.stringify(req.query));
+    var rq = req.query;
+    if (rq && rq.jobID) {
+      console.log("request for jobId = ",rq.jobID);
+      Applications.getAppsByJob(rq.jobID) 
+      .then(function(data){
+        console.log("returning application data", data);
+        res.status(200).send(JSON.stringify(data));
+      })
+      .catch(function(err){
+        console.log("could not get application data for jobID "+rq.jobID+", err:", err);
+        res.status(400).send(err);
+      })
+    } else {
+      console.log("must supply jobID in query string"); 
+      res.status(400).send("must supply jobID in query string");       
+    }
+  });
+
   router.get('/unconsideredapps', function(req, res){
     console.log('---unconsideredapps:received GET, query='+JSON.stringify(req.query));
     var rq = req.query;
--- src/jsx/app/redux/actions/app_actions.js
+++ src/jsx/app/redux/actions/app_actions.js
@@ -11,7 +11,7 @@ import { ADD_APP,
 
 function getApplications(jobID) {
 
-    return dispatch => axios.get('user/employer/appsbyjob?jobID=' + jobID)
+    return dispatch => axios.get('/user/employer/appsbyjob?jobID=' + jobID)
     .then(
       payload => dispatch({ type: FETCH_APP, payload })
     );
