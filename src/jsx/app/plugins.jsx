1a7470a40c2f0a239007c06ed633874997976214
Lance Spears <lancerspears@gmail.com>
2016-04-30 15:42:39 -0500

    -public/js/app/plugins = new imports - src/jsx/app/plugins.jsx = new imports - profile.jsx updates -delete avatars and logos -bower.json update

--- src/jsx/app/plugins.jsx
+++ src/jsx/app/plugins.jsx
@@ -25,13 +25,46 @@ import "bower_components/perfect-scrollbar/js/perfect-scrollbar.jquery.min.js";
 /*                            Optional Scripts.                             */
 /* ------------------------------------------------------------------------ */
 
-// Charts.js
+// bower
+import 'bower_components/c3/c3.js';
 import "bower_components/Chart.js/dist/Chart.js";
+import 'bower_components/d3/d3.js';
+import 'bower_components/jquery-ui/jquery-ui.min.js';
+import 'bower_components/moment/moment.js';
+import 'bower_components/trumbowyg/dist/trumbowyg.js';
+import 'bower_components/vex/js/vex.combined.min.js';
 
 
+// vendor
+import 'js/vendor/bootstrap/bootstrap.js';
+import 'js/vendor/bootstrap-datetimepicker/bootstrap-datetimepicker.js';
+import 'js/vendor/bootstrap-slider/bootstrap-slider.js';
+import 'js/vendor/datatables/datatables.js';
+import 'js/vendor/dropzone/dropzone.js';
+import 'js/vendor/fullcalendar/fullcalendar.js';
+import 'js/vendor/holder/holder.js';
+import 'js/vendor/ion.rangeSlider/ion.rangeSlider.min.js';
+import 'js/vendor/ion.tabs/ion.tabs.min.js';
+import 'js/vendor/jcrop/color.js';
+import 'js/vendor/jcrop/jcrop.js';
+import 'js/vendor/jquery-steps/jquery-steps.js';
+import 'js/vendor/jquery-validate/jquery-validate.js';
+import 'js/vendor/jquery.knob/jquery.knob.js';
+import 'js/vendor/leaflet/leaflet.js';
+import 'js/vendor/messenger/messenger.min.js';
+import 'js/vendor/morris/morris.js';
+import 'js/vendor/nestable/nestable.js';
+import 'js/vendor/prism/prism.js';
+import 'js/vendor/raphael/raphael.js';
+import 'js/vendor/select2/select2.js';
+import 'js/vendor/sparklines/sparklines.js';
+import 'js/vendor/switchery/switchery.js';
+import 'js/vendor/tablesaw/tablesaw.js';
+import 'js/vendor/timeline/timeline.js';
 
 
 /* ------------------------------------------------------------------------ */
 /*                            Default Settings.                             */
 /* ------------------------------------------------------------------------ */
+import 'js/common/rubix/rubix.js';
 import "js/common/defaults.js";
