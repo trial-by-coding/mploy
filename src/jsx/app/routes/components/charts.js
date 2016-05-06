04ea6daef74f26274987c375aa4d5ac1f436b19c
Lance Spears <lancerspears@gmail.com>
2016-05-06 15:41:16 -0500

    charts rendering data

--- src/jsx/app/routes/components/charts.js
+++ src/jsx/app/routes/components/charts.js
@@ -1,13 +1,9 @@
 import classNames from 'classnames';
 import { connect } from 'react-redux';
-// import { Link, History } from 'react-router';
 import actions from 'redux/actions';
-
 
 export default class Charts extends React.Component {
   render() {
-    const charts = this.props;
-    console.log('PIE ~>',this.props);
     return (
       <PanelContainer>
         <Panel>
@@ -21,7 +17,13 @@ export default class Charts extends React.Component {
 }
 
 export default class Body extends React.Component {
-  componentDidMount() {
+  constructor(props){
+    super(props);
+  }
+
+  componentWillMount() {
+    const charts = this.props;
+
     setTimeout(() => {
       (() => {
         var pie = Rubix.Pie('#resume-chart', {
@@ -32,44 +34,103 @@ export default class Body extends React.Component {
         pie.addData([
           {
             name: 'Applied',
-            value: 45.0,
+            value: charts.applied,
             color: '#4572a7'
           },
           {
             name: 'Considered',
-            value: 26.8,
+            value: charts.considered,
             color: '#aa4643'
           },
           {
             name: 'Denied',
-            value: 12.8,
+            value: charts.denied,
             color: '#89a54e'
           },
           {
             name: 'Offered',
-            value: 8.5,
+            value: charts.offered,
             color: '#80699b'
           },
           {
             name: 'Rescinded',
-            value: 0.7,
+            // value: charts.rescinded,
+            value: 25,  // data placeholder. Use line above when data available.
             color: '#db843d'
+          },
+          {
+            name: 'Pending',
+            // value: charts.pending,
+            value: 8.5, // data placeholder. Use line above when data available.
+            color: '#80699b'
+          },
+          {
+            name: 'Said No',
+            value: 6.2,
+            color: '#3d96ae'
+          }
+        ]);
+      })();
+
+      (() => {
+        var pie = Rubix.Pie('#jobs-chart', {
+          title: 'Jobs Chart',
+          subtitle: 'May 2016',
+          height: 300
+        });
+        pie.addData([
+          {
+            name: 'Applied',
+            value: charts.applied,
+            color: '#4572a7'
+          },
+          {
+            name: 'Considered',
+            value: charts.considered,
+            color: '#aa4643'
+          },
+          {
+            name: 'Denied',
+            value: charts.denied,
+            color: '#89a54e'
+          },
+          {
+            name: 'Offered',
+            value: charts.offered,
+            color: '#80699b'
+          },
+          {
+            name: 'Rescinded',
+            // value: charts.rescinded,
+            value: 10,  // data placeholder. Use line above when data available.
+            color: '#db843d'
+          },
+          {
+            name: 'Pending',
+            // value: charts.pending,
+            value: 17.5, // data placeholder. Use line above when data available.
+            color: '#80699b'
+          },
+          {
+            name: 'Said No',
+            value: 33,
+            color: '#3d96ae'
           }
         ]);
       })();
     }, 15);
   }
+
   render() {
-
-  const charts = this.props;
-  console.log('chart COMPONENT bottom ->', this.props);
-
     return (
       <Container id='body'>
         <Grid>
           <Row>
             <Col sm={6} collapseRight>
               <Charts id='resume-chart' />
+            </Col>
+            <Col sm={6} collapseRight>
+              <Charts id='jobs-chart' />
             </Col>
           </Row>
         </Grid>
