f3180d262e994f06f921287da49aee10d0015457
Lance Spears <lancerspears@gmail.com>
2016-05-06 15:55:42 -0500

    employer_sidebar commented out in index.js -actions and actionTypes

--- src/jsx/app/redux/actions/index.js
+++ src/jsx/app/redux/actions/index.js
@@ -1,28 +1,29 @@
-import todos from './todo_actions';
+// import todos from './todo_actions';
 import apps from './app_actions';
 import jobs from './job_actions';
 import empdash from './empdash_actions';
 import appdash from './appdash_actions';
 import profile from './profile_actions';
+import charts from './chart_actions';
 import employerunconsidered from './employer_unconsidered_actions';
 import employerconsidered from './employer_considered_actions';
 import employerinterviews from './employer_interviews_actions';
 import employeroffers from './employer_offers_actions';
 import employerposts from './employer_posts_actions';
-import employersidebar from './employer_sidebar_actions';
-import charts from './chart_actions';
+// import employersidebar from './employer_sidebar_actions';
 
 
 module.exports = {
   ...apps,
   ...jobs,
-  ...charts,
   ...empdash,
   ...appdash,
   ...profile,
+  ...charts,
   ...employerunconsidered,
   ...employerconsidered,
   ...employerinterviews,
   ...employeroffers,
   ...employerposts
+  // ...employersidebar
 };
