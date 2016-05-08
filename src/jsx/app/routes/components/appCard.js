677c7ce04c95345bbd255f6fe52fc6bc5eac8cf7
mattcphillips <matthewcannonphillips@gmail.com>
2016-05-08 14:07:09 -0500

    reconfigured app card and fixed timestamp

--- src/jsx/app/routes/components/appCard.js
+++ src/jsx/app/routes/components/appCard.js
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import moment from 'moment';
 
 import { connect } from 'react-redux'
 
@@ -33,58 +34,72 @@ export default class AppCard extends React.Component {
 
       //appCard info
       <div>
+
+        <Row className>
+          <Col xs={4} collapseRight>
+            <Img src={this.props.app.profile_picture} width='45' height='45'
+              style={{display: 'block', borderRadius: 45, border: '2px solid #fff', margin: 'auto', float: 'left'}} />
+          </Col>
+          <Col xs={8} collapseLeft id='avatar-col'>
+            <div style={{top: 23, fontSize: 28, lineHeight: 1, position: 'relative'}}>
+            {this.props.app.username}
+            </div>
+          </Col>
+        </Row>
     
         <Row style={styles}>
-          <h2> {this.props.app.username}: </h2>
-          <h4> Skills </h4>
+          <h4> Skills:</h4>
           <row className='skills'>
           { skillList.map( skill => <div className="label col-md-3 label-primary"> {skill} </div> ) }
           </row>
         </Row>
 
         <Row style={styles}>
-          <h4>Years Experience</h4>
+          <h4>Personal statement: </h4>
+          <row className='skills'>
+            { this.props.app.personal_statement }
+          </row>
+        </Row> 
+
+        <Row style={styles}>
+          <h4>Years of Relevant Experience:</h4>
           <row className='skills'>
           { this.props.app.years_experience }
           </row>
         </Row> 
 
         <Row style={styles}>
-          <h4>Education</h4>
+          <h4>Education:</h4>
           <row className='skills'>
           { this.props.app.education }
           </row>
         </Row> 
 
         <Row style={styles}>
-          <h4>Visa Required</h4>
+          <h4>Requires Visa Sponsorship</h4>
           <row className='skills'>
           { this.props.app.can_work_here === true ? 'No' : 'Yes' }
           </row>
-        </Row> 
-
-        <Row style={styles}>
-          <h4>About candidate </h4>
           <row className='skills'>
-            { this.props.app.personal_statement }
-          </row>
-          <row className='skills'>
-            <h4> Applied on </h4>
-            {this.props.app.created_at}
+            <h4> Applied on: </h4>
+            {moment(this.props.app.created_at).format('LLLL')}
           </row>
         </Row> 
 
-        <Row style={styles}>
+
+        <div style={styles}>
           <a href={this.props.app.resume}
             download={this.props.app.resume}>Download Resume</a>
+        </div>
+        <div>
           <a href={this.props.app.cover_letter}
             download={this.props.app.cover_letter}>Download Cover Letter</a>
-        </Row>
+        </div>
 
         <Row style={styles}>
           <button 
           className='btn btn-primary accept'>
-          Accept
+          Move Forward
           </button>
           <button 
           onClick={this.deleteTask}
